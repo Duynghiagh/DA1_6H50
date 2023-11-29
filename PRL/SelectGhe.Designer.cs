f64b215dcf3a519ed9186896167cbb5a244c7d54
Duynghiagh <134992743+Duynghiagh@users.noreply.github.com>
2023-11-29 11:06:59 +0700

    SELECTpHIM

---
+++ PRL/SelectGhe.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace PRL
+{
+    partial class SelectGhe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(2, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(923, 940);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(796, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 43);
+            button1.TabIndex = 0;
+            button1.Text = "Đặt Vé";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // SelectGhe
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(190, 0, 0);
+            ClientSize = new Size(937, 876);
+            Controls.Add(panel1);
+            Name = "SelectGhe";
+            Text = "SelectGhe";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+    }
+}
